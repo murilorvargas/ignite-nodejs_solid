b971b39daa2a96c7842ea8c64899c356829c8119
murilo <muriraubach@gmail.com>
2021-12-11 16:41:54 -0300

    feat: add user listing use case

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,17 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      this.listAllUsersUseCase.execute({ user_id: String(user_id) });
+
+      return response.status(201).send();
+    } catch (err) {
+      return response.status(400).json({ error: "Permission denied!" });
+    }
   }
 }
 
