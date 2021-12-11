c055fdc63659505edeed224a57e2de72f7ea6ef3
murilo <muriraubach@gmail.com>
2021-12-11 18:04:52 -0300

    fix: create user use case response

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -13,7 +13,7 @@ class CreateUserUseCase {
     const userAlreadyExists = this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error("User already exists!");
+      throw new Error("User already exists");
     }
 
     const user = this.usersRepository.create({ name, email });
